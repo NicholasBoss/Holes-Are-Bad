9c17db7df0eb3312c1457eb646b93cef7271bc4d
Benjamin Heydorn <benjamin.heydorn@gmail.com>
2022-06-14 12:54:53 -0600

    platform class

--- Casting/Platform.cs
+++ Casting/Platform.cs
@@ -4,6 +4,14 @@ namespace HolesAreBad.Casting
 {
     public class Platform : Actor
     {
-        
-    }
+        private string _platimg = Constants.IMAGE_PLATFORM;
+        private int _platwidth = Constants.PLATFORM_WIDTH;
+        private int _platheight = Constants.PLATFORM_HEIGHT;
+        public Platform()
+        {
+            SetImage(_platimg);
+            SetHeight(_platheight);
+            SetWidth(_platwidth);
+        }
+}
 }
